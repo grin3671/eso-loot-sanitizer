8f69edf25ec766fc79e6013336fe5f39e3e48e7d
grin3671 <grin3671@yandex.ru>
2023-02-21 15:38:16 +0300

    Initial commit

---
+++ src/lang/en.lua
@@ -0,0 +1,50 @@
+ZO_CreateStringId("SI_LOOTSANITIZER_NAME"                        , "LootSanitizer")
+ZO_CreateStringId("SI_LOOTSANITIZER_ACTION"                      , "destroed")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_WARNING"                     , "|cff0000Attention!|r Deleted items cannot be returned.")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL"                , "Delete trash items")
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL_NO"             , "Nope")
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL_AUTOLOOT"       , "Only at Autoloot")
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL_ALWAYS"         , "Always")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY"                 , "Chat notifications")
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY_NO"              , "Disabled")
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY_DELETE"          , "About deletion")
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY_DEV"             , "Developer mode")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_HEADER"            , "Equipment")
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_CONTROL"           , "Delete equipment")
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_CONTROL_COST"      , "Maximum cost of deleted items")
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_DESCRIPTION"       , "|cc5c29eOnly ordinary, worthless equipment in the styles of the 9 common races and in the absence of crafting features is removed.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIALMOTIF_HEADER"        , "Style materials and motifs")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL"            , "Delete style materials")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL_NO"         , "Nope")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL_COMMON"     , "Only common")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL_RARE"       , "Including rare")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL"               , "Delete motifs")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL_NO"            , "Nope")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL_COMMON"        , "Only common")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL_RARE"          , "Including rare")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIALMOTIF_DESCRIPTION"   , "|cc5c29eCommon materials and motifs are items related to the 9 common races available to players. Rare are Barbarian, Old Elven, Daedric, and Primal.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_TRAIT_HEADER"                , "Trait materials")
+ZO_CreateStringId("SI_LOOTSANITIZER_TRAIT_CONTROL"               , "Delete trait materials")
+ZO_CreateStringId("SI_LOOTSANITIZER_TRAIT_DESCRIPTION"           , "|cc5c29eOnly trait materials for armor and weapons will be removed excluding Nirnhoned.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_INGREDIENT_HEADER"           , "Ingredients")
+ZO_CreateStringId("SI_LOOTSANITIZER_INGREDIENT_CONTROL"          , "Delete ingredients")
+ZO_CreateStringId("SI_LOOTSANITIZER_INGREDIENT_CONTROL_TOOLTIP"  , "Delete ingredients of common rarity.")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_HEADER"             , "Lockpicks")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_CONTROL"            , "Delete extra lockpicks")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_SLIDER"             , "Guarded stock of lockpicks")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_SLIDER_TOOLTIP"     , "Stacks are indicated (x200).")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_DESCRIPTION"        , "|cc5c29eNew lockpicks will be removed after the specified number of stacks is accumulated in the inventory.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_HEADER"                 , "Baits")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_CONTROL"                , "Delete extra baits")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_SLIDER"                 , "Guarded stock of baits")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_SLIDER_TOOLTIP"         , "Stacks are indicated (x200).")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_DESCRIPTION"            , "|cc5c29eNew baits will be removed after the specified number of stacks is accumulated in the inventory.|r")
---
+++ src/lang/ru.lua
@@ -0,0 +1,50 @@
+ZO_CreateStringId("SI_LOOTSANITIZER_NAME"                        , "LootSanitizer")
+ZO_CreateStringId("SI_LOOTSANITIZER_ACTION"                      , "уничтожил")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_WARNING"                     , "|cff0000Внимание!|r Удалённые предметы нельзя вернуть.")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL"                , "Удалять лишние предметы")
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL_NO"             , "Нет")
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL_AUTOLOOT"       , "Только при автосборе")
+ZO_CreateStringId("SI_LOOTSANITIZER_ITEM_CONTROL_ALWAYS"         , "Всегда")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY"                 , "Уведомления в чате")
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY_NO"              , "Отключены")
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY_DELETE"          , "Только о удалении")
+ZO_CreateStringId("SI_LOOTSANITIZER_CHAT_NOTIFY_DEV"             , "Режим разработчика")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_HEADER"            , "Экипировка")
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_CONTROL"           , "Удаление экипировки")
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_CONTROL_COST"      , "Максимальная стоимость удаляемых предметов")
+ZO_CreateStringId("SI_LOOTSANITIZER_EQUIPMENT_DESCRIPTION"       , "|cc5c29eУдаляется только обычная, ничего не стоящая экипировка в стилях 9-ти стандартных рас и при отсутствии ремесленных особеностей.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIALMOTIF_HEADER"        , "Материалы стилей и мотивы")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL"            , "Удалять материалы стилей")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL_NO"         , "Нет")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL_COMMON"     , "Только стандартные")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIAL_CONTROL_RARE"       , "Включая редкие")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL"               , "Удалять ремесленные мотивы")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL_NO"            , "Нет")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL_COMMON"        , "Только стандартные")
+ZO_CreateStringId("SI_LOOTSANITIZER_MOTIF_CONTROL_RARE"          , "Включая редкие")
+ZO_CreateStringId("SI_LOOTSANITIZER_MATERIALMOTIF_DESCRIPTION"   , "|cc5c29eСтандартными считаются стили 9-ти стандартных рас, доступных игрокам. Редкими — Варварский, Древнеэльфийский, Даэдрический и Первобытный.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_TRAIT_HEADER"                , "Материалы особенностей")
+ZO_CreateStringId("SI_LOOTSANITIZER_TRAIT_CONTROL"               , "Удаление материалов особенностей")
+ZO_CreateStringId("SI_LOOTSANITIZER_TRAIT_DESCRIPTION"           , "|cc5c29eБудут удаляться только материалы особенностей для доспехов и оружия, за исключением Нирна.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_INGREDIENT_HEADER"           , "Ингредиенты")
+ZO_CreateStringId("SI_LOOTSANITIZER_INGREDIENT_CONTROL"          , "Удаление ингредиентов")
+ZO_CreateStringId("SI_LOOTSANITIZER_INGREDIENT_CONTROL_TOOLTIP"  , "Удаление ингредиентов обычной редкости.")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_HEADER"             , "Отмычки")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_CONTROL"            , "Удаление лишних отмычек")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_SLIDER"             , "Сохраняемый запас отмычек")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_SLIDER_TOOLTIP"     , "Указываются стаки (x200).")
+ZO_CreateStringId("SI_LOOTSANITIZER_LOCKPICK_DESCRIPTION"        , "|cc5c29eНовые отмычки будут удаляться, после того как в инвентаре наберётся указанное количество стаков.|r")
+
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_HEADER"                 , "Наживки")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_CONTROL"                , "Удаление лишних наживок")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_SLIDER"                 , "Сохраняемый запас наживок")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_SLIDER_TOOLTIP"         , "Указываются стаки (x200).")
+ZO_CreateStringId("SI_LOOTSANITIZER_BAIT_DESCRIPTION"            , "|cc5c29eНовые наживки будут удаляться, после того как в инвентаре наберётся указанное количество стаков.|r")
